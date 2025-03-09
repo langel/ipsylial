f0d61f912df11c0dd09972ce24c010d41ca33ad2
langel <knoxburry@gmail.com>
2025-03-09 03:53:19 -0400

    terrible logo image sprite

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,6 +3,8 @@ extends Node
 const GAME_SCENE_PATH = "res://scenes/node_2d_rng_test.tscn"
 const MAP_SCENE_PATH = "res://scenes/game.tscn"
 
+@onready var title_image = $title_image
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# start button hook it up
@@ -11,8 +13,14 @@ func _ready() -> void:
 
 	start_button.connect("pressed", Callable(self, "_on_StartButton_pressed"))
 	map_button.connect("pressed", Callable(self, "_on_MapButton_pressed"))
+	
+	title_image.position = Vector2(500,200)
+	title_image.scale = Vector2(3,3)
 
 	pass # Replace with function body.
+	
+func _draw():
+	pass
 
 func _on_StartButton_pressed():
 	# Load and switch to the game scene when the button is pressed
