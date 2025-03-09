e831fe3490d39810d62dacc54f5a9da6f97f727d
langel <knoxburry@gmail.com>
2025-03-09 19:07:40 -0400

    title screens are hard

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,5 @@
 extends Node
 
-const GAME_SCENE_PATH = "res://scenes/node_2d_rng_test.tscn"
 const MAP_SCENE_PATH = "res://scenes/game.tscn"
 
 @onready var title_image = $title_image
@@ -11,21 +10,44 @@ func _ready() -> void:
 	var start_button = $MainMenu/PlayButton
 	var map_button = $MainMenu/MapButton
 
-	start_button.connect("pressed", Callable(self, "_on_StartButton_pressed"))
 	map_button.connect("pressed", Callable(self, "_on_MapButton_pressed"))
 	
-	title_image.position = Vector2(500,200)
-	title_image.scale = Vector2(3,3)
+	var screen_size = get_viewport().size  # Get window size
+	var texture_size = title_image.texture.get_size() # Get original sprite size
+	print(screen_size.x)
+	print(screen_size.y)
+	print(texture_size)
+# Calculate scale factors
+	var scale_x = screen_size.x / texture_size.x
+	var scale_y = screen_size.y / texture_size.y
+	# Apply scale
+	title_image.scale = Vector2(scale_x, scale_y)
+	print(title_image.scale)
+	title_image.centered = false
+	title_image.position = Vector2(0,0)
+	
+	var start_label = $press_key
+	start_label.set_position(Vector2(95,300))
+	start_label.text = "press any direction to start"
+	
+	var credits_label = $credits
+	credits_label.set_position(Vector2(55,405))
+	credits_label.text = "a 7DRL submission 2025  •  code: kigu + langel  •  grafx: 32rogues by Seth"
 
 	pass # Replace with function body.
 	
 func _draw():
 	pass
+	
+func _input(event):
+	if Input.is_action_just_pressed("move_down") or Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("move_right"):	
+		var start_label = $press_key
+		start_label.text = "generating dungeon . . ."
+		$press_key.queue_redraw()
+		GameState.start_game()
+		get_tree().change_scene_to_file(MAP_SCENE_PATH)
+	
 
-func _on_StartButton_pressed():
-	# Load and switch to the game scene when the button is pressed
-	GameState.start_game()
-	get_tree().change_scene_to_file(GAME_SCENE_PATH)
 	
 func _on_MapButton_pressed():
 	# Load and switch to the game scene when the button is pressed
